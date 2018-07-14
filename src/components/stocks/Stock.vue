45cb9dece512367cc597798ae400610ed841bf08
wojtek <wojtek@tappest.com>
2018-07-14 08:29:02 +0200

    validate funds

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -14,15 +14,24 @@
             class="form-control" 
             placeholder="Quantity"
             v-model.number="quantity"
+            :class="{danger: insufficientFunds}"
             >
         </div>
         <div class="pull-right">
-          <button class="btn btn-success" @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)">
-          BUY</button></div>
+          <button class="btn btn-success" @click="buyStock" :disabled="insufficientFunds || quantity <= 0 || !Number.isInteger(quantity)">
+          {{ insufficientFunds ? 'Can\'t afford' : 'Buy' }}
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+  .danger {
+    border: 1px solid red;
+  }
+</style>
 
 <script>
   export default {
@@ -30,6 +39,14 @@
     data() {
       return {
         quantity: 0
+      }
+    },
+    computed: {
+      funds() {
+        return this.$store.getters.funds;
+      },
+      insufficientFunds() {
+        return this.quantity * this.stock.price > this.funds;
       }
     },
     methods: {
