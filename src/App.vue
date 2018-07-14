423cabbfd7c5b56348c9516bdff61d25fc7f049f
wojtek <wojtek@tappest.com>
2018-07-14 09:05:30 +0200

    add transitions to routing

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@
         <app-header></app-header>
         <div class="row">
             <div class="col-md-12">
-                <router-view></router-view>
-
+                <transition name="slide" mode="out-in">
+                    <router-view></router-view>
+                </transition>
             </div>
         </div>
     </div>
@@ -25,5 +26,32 @@
 <style>
     body {
         padding: 30px;
+    }
+    .slide-enter-active {
+        animation: slide-in 200ms ease-out forwards;
+    }
+    .slide-leave-active {
+        animation: slide-out 200ms ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateX(-30px);
+            opacity: 0;
+        }
+        to {
+            transform: translateX(0);
+            opacity: 1;
+        }
+    }
+    @keyframes slide-out {
+        from {
+            transform: translateX(0);
+            opacity: 1;
+        }
+        to {
+            transform: translateX(-30px);
+            opacity: 0;
+        }
     }
 </style>
